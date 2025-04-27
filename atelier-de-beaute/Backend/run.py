d5b46fa2aa677c193627bf4885665953e0dc3a91
MACKENA05 <mackenamuthui@gmail.com>
2025-04-27 17:06:16 +0300

    add run.py for starting the app

---
+++ atelier-de-beaute/Backend/run.py
@@ -0,0 +1,12 @@
+from app import create_app
+from app import db
+
+# Create an instance of the Flask app
+app = create_app()
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    # Start the app in debug mode for development
+    app.run(debug=True)
