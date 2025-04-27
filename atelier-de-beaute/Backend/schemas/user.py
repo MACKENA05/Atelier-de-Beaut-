071a6dcc468ac9940ba282d8b9ff1a60610fa168
MaryMachuma <machhmary173@gmail.com>
2025-04-27 16:29:36 +0300

    Add LoginSchema for user authentication

--- atelier-de-beaute/Backend/schemas/user.py
+++ atelier-de-beaute/Backend/schemas/user.py
@@ -1,3 +1,7 @@
 from marshmallow import Schema, fields, validate, ValidationError, validates
 from models.user import User, UserRole
 from email_validator import validate_email, EmailNotValidError
+
+class LoginSchema(Schema):
+    username = fields.Str(required=True)
+    password = fields.Str(required=True, load_only=True)
